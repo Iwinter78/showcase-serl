6b33c631d2e9edd07a8a963b238972d7a844c6aa
Iwinter78 <kille1404.so@gmail.com>
2024-09-09 20:46:33 +0200

    redid them with own created web components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
 import * as React from "react"
-import Link from 'next/link'
-import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu"
-import { cva } from "class-variance-authority"
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { fetchContent } from "@/data"
+import CardGrid from "@/components/card-grid";
 
-import { cn } from "@/lib/utils"
-import { Icon } from "lucide-react"
+export default async function Home() {
+  let content = await fetchContent();
 
-export default function Home() {
-  return (
-    <NavigationMenuPrimitive.Root className="flex">
-      <NavigationMenuPrimitive.Item>
-        <NavigationMenuPrimitive.Trigger className={navigationMenuTriggerStyle()}>
-          <Link href="/index">
-            Home
-          </Link>
-        </NavigationMenuPrimitive.Trigger>
-      </NavigationMenuPrimitive.Item>
-      <NavigationMenuPrimitive.Item>
-      <div className="flex w-full max-w-sm items-end space-x-2">
-        <Input type="search" placeholder="Search" />
-        <Button type="submit">Search</Button>
-      </div>
-      </NavigationMenuPrimitive.Item>
-    </NavigationMenuPrimitive.Root>
-  )
+  if (!content) {
+    return null;
+  }
+  return <CardGrid content={content} />;
 }
