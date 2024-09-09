c9ecdc8e7cfb1965fc75c52796ace8056da1d00f
Iwinter78 <kille1404.so@gmail.com>
2024-09-09 20:55:10 +0200

    added custom types

---
+++ src/types/content.ts
@@ -0,0 +1,10 @@
+export interface IContent {
+    id: number;
+    title: string;
+    content: string;
+    description: string;
+    screenshots: string[];
+    tags: string[];
+    url: string;
+    type: string;
+}
