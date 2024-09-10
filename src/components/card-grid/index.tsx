f0ed25bef089f187b7995087ad4c6fbc28062452
Iwinter78 <kille1404.so@gmail.com>
2024-09-10 11:01:26 +0200

    added filter for the select

--- src/components/card-grid/index.tsx
+++ src/components/card-grid/index.tsx
@@ -6,23 +6,27 @@ import {buttonVariants } from "@/components/ui/button"
 import { useStore } from "@tanstack/react-store";
 import { store } from "@/store";
 import { searchContent } from "@/search";
+import { filterContent } from "@/filter";
 
 interface IProps {
     content: IContent[];
 }
 
 export default function CardGrid({ content }: IProps) {
-    const searchStore = useStore(store);
-    return (
-        <div className="grid grid-cols-4 gap-4 my-10 mx-5">
-          {content.filter((item) => searchContent(item, searchStore.search)).map((item) => (
+  const searchStore = useStore(store);
+  return (
+      <div className="grid grid-cols-4 gap-4 my-10 mx-5">
+        {content
+          .filter((item) => searchContent(item, searchStore.search))
+          .filter((item) => filterContent(item, searchStore.filter))
+          .map((item) => (
             <div key={item.id} className="container mx-auto text-center">
               <strong><h1>{item.title}</h1></strong>
               <p className="text-ellipsis">{item.description}</p>
               <img className="mx-auto" width={250} height={250} src={item.screenshots[0]} />
-            <Link href={item.url} className={`${buttonVariants({ variant: "outline" })} w-full my-5`}>Read More</Link>
+              <Link href={item.url} className={`${buttonVariants({ variant: "outline" })} w-full my-5`}>Read More</Link>
             </div>
-          ))}
-        </div>
-      )
+        ))}
+      </div>
+    )
 }
