0ff21d4537444fc609346488329a03647c07ff76
Iwinter78 <kille1404.so@gmail.com>
2024-09-10 11:02:16 +0200

    added filter to store

--- src/store.ts
+++ src/store.ts
@@ -2,8 +2,10 @@ import { Store } from "@tanstack/react-store";
 
 interface IStore {
     search: string;
+    filter: string;
 }
 
 export const store = new Store<IStore>({
     search: "",
+    filter: ""
 })
