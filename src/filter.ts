e88490974df054604f65db96b4f1cfb90fd997d2
Iwinter78 <kille1404.so@gmail.com>
2024-09-10 11:02:35 +0200

    added filte filter function for select

---
+++ src/filter.ts
@@ -0,0 +1,10 @@
+import { IContent } from "./types/content";
+
+export function filterContent(item: IContent, filter: string): boolean {
+    filter = filter.toLowerCase();
+    if (filter === "all" || filter === "") {
+        return true;
+    }
+
+    return item.type === filter;
+}
