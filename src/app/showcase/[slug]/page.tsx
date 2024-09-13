9089fcc02a0566fc23c4730a8804255121bb7a32
Iwinter78 <kille1404.so@gmail.com>
2024-09-13 14:10:46 +0200

    added dynamic page for showing articles

---
+++ src/app/showcase/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import { IContent } from "@/types/content";
+import slugify from "@/utils/generateSlug";
+import { fetchContent } from "@/data";
+import Link from 'next/link'
+import { buttonVariants } from "@/components/ui/button";
+
+export default async function Page({ params }: { params: { slug: string } }) {
+    const { slug } = params;
+
+    const fetchArticle = await fetchContent();
+
+    if (!fetchArticle) return <h1>Article not found</h1>;
+
+    const article = fetchArticle.find((article: IContent) => slugify(article.title) === slug);
+
+    if (!article) return <h1>Article not found</h1>;
+    
+    return (
+        <div className="flex justify-center ">
+            <div className="flex text-center justify-center flex-col">
+                <strong>
+                    <h1 className="text-6xl">{article.title}</h1>
+                    <p className="text-center py-14">{article.description}</p>
+                </strong>
+                <article className="mx-auto w-3/6">
+                    <p className="text-center">{article.content}</p>
+                    <Link href={article.url} className={`${buttonVariants({ variant: "outline" })} w-full `}>Link to project site</Link>
+                </article>
+            </div>
+        </div>
+    )
+}
