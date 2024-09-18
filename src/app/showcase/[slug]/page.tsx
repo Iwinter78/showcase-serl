cb32fbf73423cc7ecc9606fe25f5da202b7fde16
Iwinter78 <kille1404.so@gmail.com>
2024-09-18 11:06:51 +0200

    added settings tab

--- src/app/showcase/[slug]/page.tsx
+++ src/app/showcase/[slug]/page.tsx
@@ -3,6 +3,8 @@ import slugify from '@/utils/generateSlug'
 import { fetchContent } from '@/data'
 import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/button'
+import Settings from '@/components/settings'
+
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params
@@ -18,22 +20,25 @@ export default async function Page({ params }: { params: { slug: string } }) {
   if (!article) return <h1>Article not found</h1>
 
   return (
-    <div className='flex justify-center '>
-      <div className='flex flex-col justify-center text-center'>
-        <strong>
-          <h1 className='text-6xl'>{article.title}</h1>
-          <p className='py-14 text-center'>{article.description}</p>
-        </strong>
-        <article className='mx-auto w-3/6'>
-          <p className='text-center'>{article.content}</p>
-          <Link
-            href={article.url}
-            className={`${buttonVariants({ variant: 'outline' })} mt-3 w-full `}
-          >
-            Link to project site
-          </Link>
-        </article>
+    <>
+    <Settings />
+      <div className='flex justify-center '>
+        <div className='flex flex-col justify-center text-center'>
+          <strong>
+            <h1 className='text-6xl'>{article.title}</h1>
+            <p className='py-14 text-center'>{article.description}</p>
+          </strong>
+          <article className='mx-auto w-3/6'>
+            <p className='text-center'>{article.content}</p>
+            <Link
+              href={article.url}
+              className={`${buttonVariants({ variant: 'outline' })} mt-3 w-full `}
+            >
+              Link to project site
+            </Link>
+          </article>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
