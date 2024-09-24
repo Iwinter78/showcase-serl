55eb0a24b3a05ed9607a7f77cc54b2bfb85abefc
Iwinter78 <kille1404.so@gmail.com>
2024-09-25 00:52:36 +0200

    now retrives the url and path dynamiclly. Also removed code that did not need to be there anymore because of that

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -1,8 +1,6 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 import QrCode from 'react-qr-code';
-import { usePathname } from 'next/navigation';
-
 import { IoMdSettings } from 'react-icons/io'
 
 import {
@@ -15,11 +13,6 @@ import {
 import { Button } from "@/components/ui/button"
 
 export default function Settings() {
-
-  const getPath = usePathname();
-
-  const domain = 'http://localhost:3000'; // See if this can be dynamic
-  const url = `${domain}${getPath}`;
 
   const [showQRCode, setShowQRCode] = useState(false);
 
@@ -49,7 +42,7 @@ export default function Settings() {
         {showQRCode && 
                 <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50' onClick={closeModal}>
                     <div className='bg-white p-6 rounded-lg shadow-lg' onClick={(e) => e.stopPropagation()}>
-                        <QrCode value={url} />
+                        <QrCode value={document?.location?.href} />
                         <Button className='w-full mt-2.5' onClick={closeModal}>Close</Button>
                     </div>
                 </div>
