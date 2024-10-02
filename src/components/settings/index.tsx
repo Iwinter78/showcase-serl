09e6c93a23813868d1037cb156640732b64a4b65
Iwinter78 <kille1404.so@gmail.com>
2024-10-02 14:07:24 +0200

    added now so it shows metadata to page. Hope customer likes it :)

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import QrCode from 'react-qr-code'
 import { IoMdSettings } from 'react-icons/io'
 
@@ -11,9 +11,11 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 import { Button } from '@/components/ui/button'
+import { IContent } from '@/types/content'
 
-export default function Settings() {
+export default function Settings({ article }: { article: IContent}) {
   const [showQRCode, setShowQRCode] = useState(false)
+  const [showMetaData, setShowMetaData] = useState(false)
 
   const generateQRCode = () => {
     setShowQRCode(true)
@@ -22,6 +24,15 @@ export default function Settings() {
   const closeModal = () => {
     setShowQRCode(false)
   }
+
+  const generateMetaData = () => {
+    setShowMetaData(true)
+  }
+
+  const closeMetaData = () => {
+    setShowMetaData(false)
+  }
+  console.log(article);
 
   return (
     <>
@@ -35,6 +46,7 @@ export default function Settings() {
             <DropdownMenuItem onClick={generateQRCode}>
               Generate QR Code
             </DropdownMenuItem>
+            <DropdownMenuItem onClick={generateMetaData}>Metadata</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
@@ -49,6 +61,25 @@ export default function Settings() {
           >
             <QrCode value={document?.location?.href} />
             <Button className='mt-2.5 w-full' onClick={closeModal}>
+              Close
+            </Button>
+          </div>
+        </div>
+      )}
+      {showMetaData && (
+        <div
+          className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'
+          onClick={closeMetaData}
+        >
+          <div
+            className='rounded-lg bg-white p-6 shadow-lg'
+            onClick={(e) => e.stopPropagation()}
+          >
+            <section>
+              <p className='text-black'>Tags: {article.tags.join(',')} </p>
+              <p className='text-black'>Date of publication: {article.date} </p>
+            </section>
+            <Button className='mt-2.5 w-full' onClick={closeMetaData}>
               Close
             </Button>
           </div>
