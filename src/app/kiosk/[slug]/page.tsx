bf0cc00a510ba8249bc9ef3c203c528e81dd959b
Iwinter78 <kille1404.so@gmail.com>
2024-10-03 14:41:27 +0200

    dynamic page for kiosk mode

---
+++ src/app/kiosk/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import { fetchContent } from '@/data';
+import Carusell from '@/components/carusell';
+import { IContent } from '@/types/content';
+import slugify from '@/utils/generateSlug';
+
+export default async function KioskMode({ params }: { params: { slug: string } }) {
+    let { slug } = params;
+
+    slug = slugify(slug);
+
+    const fetchArticle = await fetchContent();
+
+    if (!fetchArticle) return <h1>Article not found</h1>;
+
+    if (slug === 'all') {
+        return (
+            <Carusell fetchArticle={fetchArticle} />
+        );
+    }
+
+    const article = fetchArticle.find(
+        (article: IContent) => slugify(article.title) === slug
+      );
+
+    return (
+        <Carusell fetchArticle={article ? [article] : []} />
+    );
+}
