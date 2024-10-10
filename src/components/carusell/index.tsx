d2703dacb1c5628f89bcc7c8a002191643a46e72
Iwinter78 <kille1404.so@gmail.com>
2024-10-10 10:43:12 +0200

    changed place of if statment due to lint issue

--- src/components/carusell/index.tsx
+++ src/components/carusell/index.tsx
@@ -1,49 +1,55 @@
-'use client';
+'use client'
 
-import { IContent } from "@/types/content";
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import { IContent } from '@/types/content'
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+
+export default function Carusell({
+  fetchArticle,
+}: {
+  fetchArticle: IContent[]
+}) {
+  const [currentIndex, setCurrentIndex] = useState(0)
 
 
-export default function Carusell({ fetchArticle }: { fetchArticle: IContent[] }) {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % fetchArticle.length)
+    }, 5000)
 
-    if (fetchArticle.length === 0) {
-        return <h1>Article not found</h1>;
-    }
+    return () => clearInterval(interval)
+  }, [fetchArticle.length])
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % fetchArticle.length);
-        }, 5000);
+  if (fetchArticle.length === 0) {
+    return <h1>Article not found</h1>
+  }
 
-        return () => clearInterval(interval);
-    }, [fetchArticle.length]);
-
-    return (
-        <div className="relative w-full h-96">
-            {fetchArticle.map((article, index) => (
-                <div
-                    key={article.id}
-                    className={`absolute w-full h-full ${
-                        index === currentIndex ? "" : "hidden"
-                    }`}
-                >
-                    <div className="flex justify-center">
-                        <div className="flex justify-center items-center flex-col h-full w-3/6">
-                            <h1 className="text-6xl"><strong>{article.title}</strong></h1>
-                            <Image
-                                className="m-5 mx-auto"
-                                width={300}
-                                height={300}
-                                src={article.screenshots[0]}
-                                alt={article.title}
-                            />
-                            <p className="py-3 text-center">{article.description}</p>
-                        </div>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className='relative h-96 w-full'>
+      {fetchArticle.map((article, index) => (
+        <div
+          key={article.id}
+          className={`absolute h-full w-full ${
+            index === currentIndex ? '' : 'hidden'
+          }`}
+        >
+          <div className='flex justify-center'>
+            <div className='flex h-full w-3/6 flex-col items-center justify-center'>
+              <h1 className='text-6xl'>
+                <strong>{article.title}</strong>
+              </h1>
+              <Image
+                className='m-5 mx-auto'
+                width={300}
+                height={300}
+                src={article.screenshots[0]}
+                alt={article.title}
+              />
+              <p className='py-3 text-center'>{article.description}</p>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  )
 }
