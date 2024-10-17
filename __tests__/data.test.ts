24c873b9b3abbf996c6951c252b1f83d2a688884
Iwinter78 <kille1404.so@gmail.com>
2024-10-17 12:49:41 +0200

    added test cases. finally mocks work aswell

--- __tests__/data.test.ts
+++ __tests__/data.test.ts
@@ -1,13 +1,11 @@
-import { jest } from '@jest/globals'
-import { fetchContent } from '@/data'
-import expect from 'expect'
 import { IContent } from '@/types/content'
+import { fetchContent } from '@/data'
+import exp from 'constants';
 
 const mockData: IContent[] = [
   {
     id: 1,
     title: 'screenpipe',
-    content: 'This is the content of the screenpipe project.',
     description:
       '24/7 local AI screen & mic recording. Build AI apps that have the full context. Works with Ollama. Alternative to Rewind.ai. Open. Secure. You own your data. Rust.',
     screenshots: ['https://screenpi.pe/1024x1024.png'],
@@ -24,19 +22,50 @@ const mockData: IContent[] = [
     type: 'student',
     date: '2023-10-02',
   },
-]
-jest.mock('@/data', () => ({
-  fetchContent: jest.fn(() => Promise.resolve(mockData)),
-}))
-//test('If it returns the correct data from file', async () => {
-//    const mock = jest.fn<() => Promise<IContent[] | null>>().mockResolvedValue(mockData);
-//
-//    const data = await mock();
-//    expect(data).toEqual(mockData);
-//});
+];
 
-test('If it returns the correct data from file', async () => {
-  const data = await fetchContent()
-  console.log(data)
-  expect(data).toEqual(mockData)
+jest.mock('fs', () => ({
+  promises: {
+    readFile: jest.fn(() => Promise.resolve(JSON.stringify(mockData))),
+  },
+}))
+
+describe('Testing of fetchContent', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Should return the correct data from file', async () => {
+    const data = await fetchContent();
+    expect(data).toEqual(mockData);
+    expect(data).toHaveLength(1);
+  })
+
+  it('Should return the correct data from file', async () => {
+    const data = await fetchContent();
+    expect(data).toEqual(mockData);
+    expect(data).toHaveLength(1);
+    expect(data?.[0]).toBeDefined();
+    expect(data?.[0].id).not.toBeNull();
+    expect(data?.[0].id).toBe(1);
+    expect(data?.[0].title).toBe('screenpipe');
+    expect(data?.[0].description).toBe(
+      '24/7 local AI screen & mic recording. Build AI apps that have the full context. Works with Ollama. Alternative to Rewind.ai. Open. Secure. You own your data. Rust.'
+    );
+    expect(data?.[0].screenshots).toEqual([
+      'https://screenpi.pe/1024x1024.png',
+    ]);
+    expect(data?.[0].tags).toEqual([
+      'machine-learning',
+      'ai',
+      'computer-vision',
+      'ml',
+      'vision',
+      'multimodal',
+      'llm',
+    ]);
+    expect(data?.[0].url).toBe('https://github.com/mediar-ai/screenpipe');
+    expect(data?.[0].type).toBe('student');
+    expect(data?.[0].date).toBe('2023-10-02');
+  });
 })
