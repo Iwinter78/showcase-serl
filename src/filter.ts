7e19f72cd663181126aa76469e1450d1a58a79d8
Iwinter78 <kille1404.so@gmail.com>
2024-10-17 12:50:21 +0200

    fixed bug where you could insert something else then a string

--- src/filter.ts
+++ src/filter.ts
@@ -1,10 +1,10 @@
 import { IContent } from './types/content'
 
 export function filterContent(item: IContent, filter: string): boolean {
-  filter = filter.toLowerCase()
+  filter = `${String(filter).toLowerCase()}`;
   if (filter === 'all' || filter === '') {
-    return true
+    return true;
   }
 
-  return item.type === filter
+  return item.type === filter;
 }
