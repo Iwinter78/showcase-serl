ac52d1b64943f7cf3ef4fb78503c0b9ffc2175c7
Iwinter78 <kille1404.so@gmail.com>
2024-10-21 15:19:07 +0200

    added suspense for loading spinner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import type { Metadata } from 'next'
 import './globals.css'
 import Navbar from '@/components/navbar'
@@ -17,11 +17,14 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
+        
         <div className='flex h-screen flex-col justify-between'>
+        <Suspense fallback={<h1>Loading...</h1>}>
           <header className='h-10'>
             <Navbar />
           </header>
-          <main className='h-30 mb-auto'>{children}</main>
+            <main className='h-30 mb-auto'>{children}</main>
+          </Suspense>
           <footer className='h-10'>
             <p className='text-center'>
               &copy; {new Date().getFullYear()} ShowCaseSERL
